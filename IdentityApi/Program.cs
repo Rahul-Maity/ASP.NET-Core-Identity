c0d19524631566d5947baf2782f40ce0b80259bb
Rahul <mrahulmaity623@gmail.com>
2024-07-16 16:23:52 +0530

    Added authentication service and pipeline

--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,11 +1,17 @@
 
 
 using IdentityApi.Data;
+using IdentityApi.Models;
+using IdentityApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +26,45 @@ builder.Services.AddDbContext<Context>(options =>
 
     options.UseNpgsql(connectionString)
 );
-    
+
+
+//configuring identity services
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+
+    options.SignIn.RequireConfirmedEmail = true;
+
+})
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<Context>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddUserManager<UserManager<User>>()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.AddScoped<JWTService>();
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:key"])),
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidateIssuer = true,
+            ValidateAudience = false
+        };
+    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,7 +75,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ IdentityApi/Services/JWTService.cs
@@ -0,0 +1,48 @@
+﻿using IdentityApi.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityApi.Services
+{
+    public class JWTService
+    {
+        private readonly IConfiguration _config;
+        private readonly SymmetricSecurityKey _jwtkey;
+
+        public JWTService(IConfiguration config)
+        {
+            _config = config;
+            _jwtkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:key"]));
+        }
+        public string createJWT(User user)
+        {
+            var userClaims = new List<Claim>
+            {
+                 new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.GivenName,user.FirstName),
+                new Claim(ClaimTypes.Surname,user.LastName)
+            };
+            var credentials = new SigningCredentials(_jwtkey, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(userClaims),
+                Expires = DateTime.UtcNow.AddDays(int.Parse(_config["JWT:ExpiresInDays"])),
+                SigningCredentials = credentials,
+                Issuer = _config["JWT:Issuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwt = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(jwt);
+                
+        }
+    }
+}

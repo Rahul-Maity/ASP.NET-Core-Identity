9f48f578abcf270043833908a3800e25695159f2
Rahul <mrahulmaity623@gmail.com>
2024-07-20 08:18:05 +0530

    client-side & server-side validation for login added

--- ClientApp/src/app/account/account.service.ts
+++ ClientApp/src/app/account/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Register } from '../shared/models/register';
 import { environment } from '../../environments/environment.development';
+import { Login } from '../shared/models/login';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ export class AccountService {
 
 
   constructor(private http: HttpClient) { }
+
+  login(model: Login) {
+    return this.http.post(`${environment.appUrl}/api/Account/login`, model);
+  }
   register(model: Register) {
    return this.http.post(`${environment.appUrl}/api/Account/register`, model);
   }
--- ClientApp/src/app/account/login/login.component.ts
+++ ClientApp/src/app/account/login/login.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+  submitted: boolean = false;
+  errorMessage: string[] = [];
+  constructor(private fb: FormBuilder, private accountService:AccountService) { }
+  ngOnInit(): void {
+    this.initializeForm();
+  }
 
+
+  initializeForm() {
+    this.loginForm = this.fb.group({
+
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+
+    })
+  }
+
+  login()
+  {
+    this.submitted = true;
+    this.errorMessage = [];
+    if (this.loginForm.invalid) {
+      this.accountService.login(this.loginForm.value).subscribe({
+        next:(response :any)=> {
+          
+        },
+        error:error => {
+          if (error.error.errors) { this.errorMessage = error.error.errors }
+          else {
+            this.errorMessage.push(error.error);
+          }
+        }
+      }
+        
+      )
+    }
+    
+  }
 }
